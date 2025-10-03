de7b0d7353d7ed444ebfc39d3443a47c02cdd620
Shrishti Srivastava <srivastava123.shrishti@gmail.com>
2025-10-03 23:54:43 +0530

    authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,51 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "../components/Navbar";
+import ResumeCard from "../components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+import type { Route } from "./+types/home";
+import { resumes } from "../constants/index";
+import { usePuterStore } from "~/lib/puter";
+import { useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
+
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "OptiHire" },
+    { name: "description", content: "Get Feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>
+          Track Your Applications & Resume Ratings
+        </h1>
+        <h2>
+          Review your submisssion and check AI-powered feedback
+        </h2>
+      </div>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes && resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+
+
+  </main>
 }
